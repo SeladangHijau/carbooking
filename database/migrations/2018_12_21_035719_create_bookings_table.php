b0ecceae9246cdefa6d3df0ccce8cc83ee68307d
Nadzmiidzham <nadzmiidzham@gmail.com>
2018-12-20 23:31:26 -0800

    Initial commit

---
+++ database/migrations/2018_12_21_035719_create_bookings_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBookingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bookings', function (Blueprint $table) {
+            $table->increments('id');
+            $table->bigInteger('user_id');
+            $table->bigInteger('driver_id');
+            $table->string('point_a');
+            $table->string('point_b');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bookings');
+    }
+}
