f1da4911d8ab520c6951550a69c30fe40981b2eb
nadzmiidzham <nadzmiidzham@gmail.com>
2018-12-26 09:44:51 +0800

    minor changes

--- app/Car.php
+++ app/Car.php
@@ -7,16 +7,4 @@ use Illuminate\Database\Eloquent\Model;
 class Car extends Model
 {
     protected $fillable = [ 'driver_id', 'model', 'color', 'plate_no' ];
-
-    public function saveCar(Car $car) {
-        return 'Saved';
-    }
-
-    public function editCar(Car $car) {
-        return 'Edited';
-    }
-
-    public function deleteCar(Car $car) {
-        return 'Deleted';
-    }
 }
